1e2fff8454d4daa62b4a7c0683a12d4844cb1d0d
허현 <nsksjsk2@hanmail.net>
2023-12-26 19:17:52 +0900

    final

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 
-searching = '합정 스타벅스'
+searching = '홍대 스타벅스'
 url = 'https://dapi.kakao.com/v2/local/search/keyword.json?query={}'.format(searching)
 headers = {
     "Authorization": "KakaoAK cd12239e86cd67a7fdc5c228fe77162d"
