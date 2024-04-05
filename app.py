6f13f5ee68c345c894cbc62f09ba277669baa052
hurhyeon <90999300+hurhyeon@users.noreply.github.com>
2024-04-05 15:51:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 
 searching = '홍대 스타벅스'
+
 url = 'https://dapi.kakao.com/v2/local/search/keyword.json?query={}'.format(searching)
 headers = {
     "Authorization": "KakaoAK cd12239e86cd67a7fdc5c228fe77162d"
